3cc735fd817c9b75405c2d873f28e0cbff4f2203
JenniferFadare <70460125+JenniferFadare@users.noreply.github.com>
2020-11-25 01:44:21 -0600

    fixed syntax errors that prevented game from starting properly

--- assets/js/game.js
+++ assets/js/game.js
@@ -81,8 +81,8 @@ var fightOrSkip = function () {
         }
         else { 
             return false;
-    }
-        
+        }
+    }       
 }
 
 var fight = function(enemy) {
@@ -93,40 +93,40 @@ var fight = function(enemy) {
     }
     while (playerInfo.health > 0 && enemy.health > 0) {
            if (fightOrSkip()) {
-               break;
+            break;
            }
-            
-            //Subtract the value of `playerInfo.attack` from the value of `enemy.health` and use that result to update the value in the `enemy.health` variable
-             var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
-             enemy.health = Math.max(0, enemy.health - damage);
-             // Log a resulting message to the console so we know that it worked.
-             console.log( playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining.");
-       
-             //check enemy's health
-            if (enemy.health <= 0) {
-                window.alert(enemy.name + " has died!");
-                playerInfo.money = playerInfo.money + 20;
-                break; 
-             }
+           else {
+                var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+                enemy.health = Math.max(0, enemy.health - damage);
+                console.log( playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining.");
+        
+                //check enemy's health
+                if (enemy.health <= 0) {
+                    window.alert(enemy.name + " has died!");
+                    playerInfo.money = playerInfo.money + 20;
+                    break; 
+                }
 
-            else {
-            window.alert(enemy.name + " still has " + enemy.health + " health left.");
-             }
-      
-             // remove player's health by subtracting the amount set in the enemy.attack variable
-             var damage = randomNumber(enemy.attack - 3, enemy.attack);
-             playerInfo.health = Math.max (0, playerInfo.health - damage);
-             // Log a resulting message to the console so we know that it worked.
-             console.log(enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + enemy.health + " health remaining.");
-    
-             // check player's health
-            if (playerInfo.health <= 0) {
-                window.alert(playerInfo.name + " has died!");
-                break;
+                else {
+                window.alert(enemy.name + " still has " + enemy.health + " health left.");
+                }
+        
+                // remove player's health by subtracting the amount set in the enemy.attack variable
+                var damage = randomNumber(enemy.attack - 3, enemy.attack);
+                playerInfo.health = Math.max (0, playerInfo.health - damage);
+                // Log a resulting message to the console so we know that it worked.
+                console.log(enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + enemy.health + " health remaining.");
+        
+                // check player's health
+                if (playerInfo.health <= 0) {
+                    window.alert(playerInfo.name + " has died!");
+                    break;
+                }
+                else {
+                    window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+                }
             }
-            else {
-                window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
-            }
+ 
     }
     shop();
 }
